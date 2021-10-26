2916279056d387e1cc6aaeca288ce408f8cf290d
Eggon <ec_rejestracje@pro.wp.pl>
2021-10-26 23:28:18 +0200

    predeploy.bat

--- predeploy.bat
+++ predeploy.bat
@@ -1,4 +1,4 @@
 del Contents\mods\eggonsHotkeys\media\lua\shared\EHK_ReappearAction.lua
 del Contents\mods\eggonsHotkeys\media\lua\shared\TestAction.lua
 del Contents\mods\eggonsHotkeys\media\lua\client\testingModuleTemplate.lua
-del Contents\mods\eggonsHotkeys\media\lua\client\EHK?Debug.lua
+del Contents\mods\eggonsHotkeys\media\lua\client\EHK_Debug.lua
